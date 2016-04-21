5ab5bc0585bbd851f7bb2d9ce7fed3d5d0d3930c
Oliver <oliverbarnum32@gmail.com>
2016-04-21 14:09:06 -0400

    Fix water resolution

--- public/core.js
+++ public/core.js
@@ -15,7 +15,7 @@ function($scope,$http){
 	];
 
 	$scope.modes = [
-		{name:"Geo",     modes : ["Depth","Height","Tectonic","Satellite"]},
+		{name:"Geo",     modes : ["Depth","Elevation","Height","Tectonic","Satellite"]},
 		{name:"Weather", modes : ["Sunlight", "Rainfall"]},
 		{name:"Soil",    modes : ["Nutro", "Nucium", "Nutrients"]},
 		{name:"Plant",   modes : ["Nutro Stores", "Nucium Stores", "Water Stores", "Nutrient Stores"]}
