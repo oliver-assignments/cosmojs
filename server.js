c27ad6ea1e9ba62c70f183f72841430f56b87349
Oliver <oliverbarnum32@gmail.com>
2016-09-15 09:59:36 -0400

    Use soil-scape module

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var favicon = require('serve-favicon');
-var mongo = require('mongodb').MongoClient;
 var assert = require('assert');
 
 var app = express();
@@ -17,25 +16,13 @@ app.get('/', function(req,res)
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-var cosmo = require('./scripts/cosmo/index.js');
-
-// mongo.connect("mongodb://<username>:<password>@ds145295.mlab.com:45295/heroku_56dj7qzn", function(err, db) {
-
-//   var cursor = db.collection('worlds').find();
-//   cursor.each(function(err,doc)
-//   {
-//     if(err)
-//       console.log("err" + err);
-//     console.dir(doc);
-//     db.close();
-//   });
-// });
+var cosmo = require('./src/soil-scape/index.js');
 
 //  Routes
-require('./scripts/requestRoutes')(app,cosmo);
-require('./scripts/managementRoutes')(app,cosmo);
-require('./scripts/renderRoutes')(app,cosmo);
-require('./scripts/utilityRoutes')(app,cosmo);
+require('./src/requestRoutes')(app,cosmo);
+require('./src/managementRoutes')(app,cosmo);
+require('./src/renderRoutes')(app,cosmo);
+require('./src/utilityRoutes')(app,cosmo);
 
 var port = Number(process.env.PORT || 3000);
 app.listen(port);
